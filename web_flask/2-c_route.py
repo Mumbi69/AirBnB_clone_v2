512ceb036eed5621bde95132d5518cd73889152a
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-22 19:22:25 +0000

    AirBnB clone - Web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,8 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """C is fun"""
-    text = text.replace('_', ' ' )
-    return f'C {text}'
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
