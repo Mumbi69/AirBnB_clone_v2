25c3c18d017f9fbeb65ce754a6dd3ef4b61410e1
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-23 11:17:27 +0000

    AirBnB clone - Web framework

--- models/state.py
+++ models/state.py
@@ -3,10 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
